55fdc418fdb7b80170da9fb53207b667fb3fe5ae
murdocif <pyalex@gmail.com>
2014-01-10 18:14:09 +0200

    New project added

---
+++ MySerialization/SerializationUsage/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializationUsage
+{
+    public class Program2
+    {
+        public static void Print()
+        {
+            Console.WriteLine("Hello!");
+        }
+    }
+}
